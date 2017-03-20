53bbc4c87716047131fd2afa53111114cabb5a63
Torbjørn Marø <torbjorn.maro@gmail.com>
2017-03-20 23:46:45 +0100

    Util tests

--- t/cl-hipchat.util.lisp
+++ t/cl-hipchat.util.lisp
@@ -4,10 +4,30 @@
   (:use cl prove cl-hipchat.util))
 (in-package cl-hipchat.util-test)
 
-(plan nil)
+(plan 9)
 
-(ok (not (find 4 '(1 2 3))))
-(is 4 4)
-(isnt 1 #\1)
+
+(ok (in-range-p 1 1 1))
+(ok (in-range-p 5 0 10))
+(ok (not (in-range-p 10 0 5)))
+(ok (not (in-range-p 0 1 10)))
+
+
+(is (keyword-to-lowercase-string :foo) "foo")
+(isnt (keyword-to-lowercase-string :foo) "FOO")
+
+
+(is (append-query-params "index.html" '(("foo" . 1)))
+    "index.html?foo=1")
+
+(is (append-query-params "index.html" '(("foo" . 1)
+                                        ("bar" . "hello")
+                                        ("zot" . "world")))
+    "index.html?foo=1&bar=hello&zot=world")
+
+
+(is (room-resource "A Test Room")
+    "room/A%20Test%20Room")
+
 
 (finalize)
